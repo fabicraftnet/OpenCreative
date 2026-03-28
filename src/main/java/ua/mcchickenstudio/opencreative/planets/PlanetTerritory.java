3e25191c83de86b4aebb37ee76b8dfe1faf85a48
nosatlev <mcchickenstudio@gmail.com>
2026-03-28 20:00:37 +0200

    Experiment: Using world.save(false) for new Minecraft versions

--- src/main/java/ua/mcchickenstudio/opencreative/planets/PlanetTerritory.java
+++ src/main/java/ua/mcchickenstudio/opencreative/planets/PlanetTerritory.java
@@ -40,6 +40,7 @@ import ua.mcchickenstudio.opencreative.utils.world.generators.StructuresCapable;
 import ua.mcchickenstudio.opencreative.utils.world.generators.WorldGenerator;
 import ua.mcchickenstudio.opencreative.utils.world.generators.WorldGenerators;
 
+import java.lang.reflect.Method;
 import java.util.*;
 
 import static ua.mcchickenstudio.opencreative.utils.FileUtils.getPlanetConfig;
@@ -238,9 +239,15 @@ public class PlanetTerritory {
         if (world != null) {
             if (asyncSaveData) {
                 for (Chunk chunk : world.getLoadedChunks()) {
-                    chunk.unload(autoSave);
+                    world.unloadChunk(chunk.getX(), chunk.getZ(), autoSave);
                 }
-                world.save();
+                try {
+                    // 1.21+ Content:
+                    Method saveMethod = world.getClass().getMethod("save", boolean.class);
+                    saveMethod.invoke(world, false);
+                } catch (Exception ignored) {
+                    world.save();
+                }
                 busy = true;
                 Bukkit.getScheduler().runTaskLater(OpenCreative.getPlugin(), () -> {
                     Bukkit.unloadWorld(planet.getWorldName(), false);
